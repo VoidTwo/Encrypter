fc44955171bc3a6cff66ed8e3a5b18d702a9a6b8
VoidTwo <27699867+VoidTwo@users.noreply.github.com>
2022-08-28 18:13:45 -0500

    Achieve more robust file handling and fix issues
    Implemented better cleanup of attained resources and smarter symlink
    detection (if needed). Also, did some mild refactoring.

--- encrypter/file_operations.py
+++ encrypter/file_operations.py
@@ -5,11 +5,11 @@ from __future__ import annotations
 
 # Standard imports
 from os import (
+    close as os_close,
     fdopen as os_fdopen,
     fstat as os_fstat,
     lstat as os_lstat,
     open as os_open)
-from sys import exit as sys_exit
 from typing import TYPE_CHECKING
 
 # Local imports
@@ -24,14 +24,12 @@ if TYPE_CHECKING:
         Optional)
 
     # Local imports
-    from .custom_types import (
-        LiteralFalse,
-        LiteralPlatform)
+    from .custom_types import LiteralFalse
 
 
-def get_secure_open_flags() -> int:
-    platform: LiteralPlatform = Platform().platform
+def get_secure_open_properties() -> tuple[int, bool]:
     flags: int = 0
+    no_follow: bool = False
 
     try:
         from os import O_EXLOCK
@@ -43,6 +41,7 @@ def get_secure_open_flags() -> int:
     try:
         from os import O_NOFOLLOW
         flags |= O_NOFOLLOW
+        no_follow = True
         del O_NOFOLLOW
     except ImportError:
         pass
@@ -54,41 +53,65 @@ def get_secure_open_flags() -> int:
     except ImportError:
         pass
 
-    if platform == 'W':
+    if Platform.platform == 'W':
         from os import O_BINARY
         flags |= O_BINARY
         del O_BINARY
-    return flags
+    return flags, no_follow
+
+
+def symlink_testing(fd: int, filename: str, *, iterations: int = 10) -> bool:
+    for _ in range(iterations):
+        if os_fstat(fd).st_dev != os_lstat(filename).st_dev \
+                or os_fstat(fd).st_ino != os_lstat(filename).st_ino:
+            return True
+    return False
 
 
 class SecureOpen:
-    __flags: int = get_secure_open_flags()
-
     __slots__ = (
-        '__filename', '__file')
+        '__filename', '__file_descriptor', '__file',
+        '__flags', '__no_follow')
 
-    def __init__(self, filename: str = 'encrypter.db') -> None:
+    def __init__(self, filename: str = './encrypter.db') -> None:
         self.__filename: str = filename
+        self.__file_descriptor: Optional[int] = None
         self.__file: Optional[BinaryIO] = None
+
+        self.__flags: int
+        self.__no_follow: bool
+        self.__flags, self.__no_follow = get_secure_open_properties()
         return
 
     def __enter__(self) -> BinaryIO:
-        if self.__file is not None:
+        if (self.__file_descriptor, self.__file) != (None, None):
+            self.close()
             raise AssertionError('File is already open.')
 
-        fd: int = os_open(self.__filename, flags=self.__flags, mode=0o444)
+        self.__file_descriptor = os_open(self.__filename, flags=self.__flags, mode=0o444)
 
-        # Best we can do to prevent opening symlinks
-        if os_fstat(fd).st_dev != os_lstat(self.__filename).st_dev:
-            sys_exit(1)
+        if not self.__no_follow:
+            if symlink_testing(self.__file_descriptor, self.__filename):
+                self.close()
+                raise AssertionError(f'Symlink detected on file "{self.__filename}".')
 
-        if os_fstat(fd).st_ino != os_lstat(self.__filename).st_ino:
-            sys_exit(1)
-
-        self.__file = os_fdopen(fd, 'rb')
+        self.__file = os_fdopen(self.__file_descriptor, 'rb')
         return self.__file
 
     def __exit__(self, *_: Any) -> LiteralFalse:
-        if not self.__file.closed:
-            self.__file.close()
+        self.close()
         return False
+
+    def close(self) -> None:
+        if self.__file is not None and not self.__file.closed:
+            self.__file.close()
+            self.__file = None
+
+        if self.__file_descriptor is not None:
+            try:
+                os_close(self.__file_descriptor)
+            except OSError:
+                pass
+
+            self.__file_descriptor = None
+        return
