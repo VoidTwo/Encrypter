ae19bf3b41a5134dfbd255139d33d38b8a4cdc04
VoidTwo <27699867+VoidTwo@users.noreply.github.com>
2022-08-31 16:27:08 -0500

    Add CustomException class
    Added custom exceptions to be used for convenient and "smarter" custom
    error handling.

--- encrypter/common_utils.py
+++ encrypter/common_utils.py
@@ -4,12 +4,18 @@
 from __future__ import annotations
 
 # Standard imports
+from traceback import format_exception as tb_format_exception
 from typing import TYPE_CHECKING
 
 # Type checking
 if TYPE_CHECKING:
+    # Standard imports
+    from typing import Optional
+
     # Local imports
-    from .custom_types import LiteralPlatform
+    from .custom_types import (
+        LiteralExceptionType,
+        LiteralPlatform)
 
 
 def get_platform() -> LiteralPlatform:
@@ -28,3 +34,40 @@ def get_platform() -> LiteralPlatform:
 
 class Platform:
     platform: LiteralPlatform = get_platform()
+
+
+class CustomException(Exception):
+    __slots__ = (
+        '__exception_type', '__description', '__exception')
+
+    def __init__(
+            self,
+            exception_type: LiteralExceptionType,
+            description: Optional[str] = None,
+            exception: Optional[Exception] = None) \
+            -> None:
+        self.__exception_type: LiteralExceptionType = exception_type
+        self.__description: Optional[str] = description
+        self.__exception: Optional[Exception] = exception
+        return
+
+    def __str__(self) -> str:
+        error_data: list[str] = [self.__exception_type]
+
+        if self.__description is not None:
+            error_data.append(f': {self.__description}')
+
+        if self.__exception is not None:
+            exception_stack_trace: str = ''.join(tb_format_exception(self.__exception))
+            error_data.append(f'\n\n{exception_stack_trace}')
+        return ''.join(error_data)
+
+    def __repr__(self) -> str:
+        description: str = 'NONE' if self.__description is None else self.__description
+        exception: str = 'NONE' if self.__exception is None else str(type(self.__exception))
+
+        representation: str = \
+            f'Exception Type: {self.__exception_type}\n' \
+            f'Description: {description}\n' \
+            f'Exception: {exception}'
+        return representation
