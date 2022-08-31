8c720a63afe28e46ba06ae4b5e8c5a6513f447c0
VoidTwo <27699867+VoidTwo@users.noreply.github.com>
2022-08-31 02:25:24 -0500

    Update version & package data

--- encrypter/__init__.py
+++ encrypter/__init__.py
@@ -4,9 +4,11 @@
 from .common_utils import Platform
 from .cryptography_utils import (
     CharacterSets,
+    secure_decrypt,
     secure_encrypt,
     secure_random_string)
 from .custom_types import LiteralPlatform
+from .data_modules import EncrypterDatabase
 from .file_operations import (
     SecureOpen,
     SecureWriter)
@@ -18,12 +20,14 @@ __description__ = 'Secure offline password vault using modern cryptographically 
                   'algorithms'
 __author__ = 'VoidTwo'
 __license__ = 'AGPL-3.0-only'
-__version__ = '0.0.3-alpha'
+__version__ = '0.0.4-alpha'
 
 __all__ = (
     'CharacterSets',
+    'EncrypterDatabase',
     'LiteralPlatform',
     'Platform',
+    'secure_decrypt',
     'secure_encrypt',
     'secure_random_string',
     'SecureOpen',
