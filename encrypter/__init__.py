43f61fb0e48c5bb95414d8493ec1933924886edc
VoidTwo <27699867+VoidTwo@users.noreply.github.com>
2022-08-30 22:52:27 -0500

    Update version

--- encrypter/__init__.py
+++ encrypter/__init__.py
@@ -18,7 +18,7 @@ __description__ = 'Secure offline password vault using modern cryptographically 
                   'algorithms'
 __author__ = 'VoidTwo'
 __license__ = 'AGPL-3.0-only'
-__version__ = '0.0.2-alpha'
+__version__ = '0.0.3-alpha'
 
 __all__ = (
     'CharacterSets',
