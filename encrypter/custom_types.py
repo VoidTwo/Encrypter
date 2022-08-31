e51b0dfeffd1248a9493190ee09b7522bf1d71cc
VoidTwo <27699867+VoidTwo@users.noreply.github.com>
2022-08-31 02:15:41 -0500

    Add rudimentary Encrypter database types
    Added `TypedDict`s that may compose the Encrypter database structure.
    This is a basic start and should not be seen as the final design.

--- encrypter/custom_types.py
+++ encrypter/custom_types.py
@@ -1,8 +1,31 @@
 # -*- coding: utf-8 -*-
 
 # Standard imports
-from typing import Literal
+from typing import (
+    Any,
+    Literal,
+    Optional,
+    Type,
+    TypedDict)
 
 
 LiteralFalse = Literal[False]
 LiteralPlatform = Literal['L', 'W', 'M', 'U']
+
+TypedDictEncrypterDatabaseSecret: Type = TypedDict(
+    'TypedDictEncrypterDatabaseSecret', {
+        'title': str,
+        'comments': Optional[str],
+        'description': Optional[str],
+        'secret': str})
+
+TypedDictEncrypterDatabaseGlobalSettings: Type = TypedDict(
+    'TypedDictEncrypterDatabaseGlobalSettings', {
+        'name': Optional[str]})
+
+TypedDictEncrypterDatabase: Type = TypedDict(
+    'TypedDictEncrypterDatabase', {
+        'folders': dict[str, Any],
+        'passwords': list[TypedDictEncrypterDatabaseSecret],
+        'settings': TypedDictEncrypterDatabaseGlobalSettings,
+        'tags': list[str]})
